ebee31a763b9e159afe0b0eb141e13b077ae12b2
ViolaRafael <rafaelviola.eprosec@gmail.com>
2023-01-06 10:39:53 -0100

    Add form files

--- lib/screens/place_form_screen.dart
+++ lib/screens/place_form_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/widgets/image_input.dart';
 
 class PLaceFormScreen extends StatefulWidget {
   const PLaceFormScreen({Key? key}) : super(key: key);
@@ -8,14 +9,52 @@ class PLaceFormScreen extends StatefulWidget {
 }
 
 class _PLaceFormScreenState extends State<PLaceFormScreen> {
+  final _titleController = TextEditingController();
+  void _submitForm() {
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Novo Lugar'),
       ),
-      body: Center(
-        child: Text('form'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _titleController,
+                      decoration: const InputDecoration(
+                        labelText: 'Título',
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const ImageInput(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Text('teste'),
+          ElevatedButton.icon(
+            onPressed: _submitForm,
+            icon: const Icon(Icons.add),
+            label: const Text('Adicionar'),
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.black,
+              backgroundColor: Theme.of(context).colorScheme.secondary,
+              elevation: 0,
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            ),
+          ),
+        ],
       ),
     );
   }
