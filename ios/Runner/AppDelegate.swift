a9dd519a08e815616404645af016c0e4afa29788
ViolaRafael <rafaelviola.eprosec@gmail.com>
2023-01-11 11:12:19 -0100

    Hide Google Api Keys

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,19 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCKggKvIgOH83BQ0WJbjK4kP82CvcB9wkQ")
+      var keys: NSDictionary?
+
+      if let path = Bundle.main.path(forResource: "Keys", ofType: "plist") {
+              keys = NSDictionary(contentsOfFile: path)
+          }
+      if let dict = keys {
+          let ApiKey = dict["GoogleApiKey"] as? String
+          // Initialize Parse.
+          Parse.setApiKey(ApiKey!)
+      }
+
+
+    GMSServices.provideAPIKey(ApiKey)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
